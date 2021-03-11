f4121159d467987e0e5ebf54b96f9b419b1c0a49
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2021-03-10 19:48:34 -0600

    third commit

--- script.js
+++ script.js
@@ -1,13 +1,66 @@
-// const numbers = document.getElementsByClassName('number');
-// const operators = document.getElementsByClassName('operator');
-// const clearElements = document.getElementById('clear');
-// const deleteElements = document.getElementById('delete');
-const displayElement = document.getElementById('input');
-
 //get number to appear after pressing button 
 
-document.getElementsByClassName("number").forEach(numberButton => {
+const displayElement = document.getElementById('input');
+const numbers = document.querySelectorAll('.number');
+const numberInput = numbers.textContent
+
+let firstInput = '';
+let operator = null;
+let secondInput = '';
+let equalTo = false;
+
+
+numbers.forEach(numberButton => {
   numberButton.addEventListener('click', (e) => {
     const numberInput = e.target.textContent;
-  })
+    displayElement.value += numberInput
+
+    // if(firstInput && !operator && equalTo) {
+    //   firstInput = '';
+    //   equalTo = false;
+    // }
+
+    // if (!operator) {
+    //   firstInput += numberInput;
+    //   displayElement.value = firstInput
+    // }
+
+    // if (firstInput && operator) {
+    //   secondInput += numberInput;
+    //   displayElement.value = secondOperand;
+    // }
+  })   
 });
+
+
+// delete numbers
+
+const deleteInput = document.getElementById('delete');
+
+deleteInput.addEventListener('click', (e) => {
+  displayElement.value = displayElement.value.slice(0, -1)
+})
+
+//reset to 0
+
+const clearInput = document.getElementById('clear')
+
+clearInput.addEventListener('click', (e) => {
+  const clear =  0;
+  displayElement.value = clear
+});
+
+// calculate numbers 
+
+const newDisplay = document.createElement('input');
+const addition = document.getElementById('add');
+
+addition.addEventListener('click', (e) => {
+  return numberInput.displayElement + numberInput.newDisplay
+});
+
+
+
+
+
+
